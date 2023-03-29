1f303ab303a0b8e54f5c3daf70ba4e6318984d22
Kingkarn <Kingkarn.jant@mail.kmutt.ac.th>
2023-03-29 14:20:32 +0700

    save1

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program {
+    static void Main(string[] args) {
+       Console.Write("Input N: ");
+       int N = int.Parse(Console.ReadLine()); 
+
+       Console.Write("Input K: ");
+       int K = int.Parse(Console.ReadLine());
+
+       Console.Write("Input gender: ");
+       int gender = int.Parse(Console.ReadLine());
+
+       Console.Write("Input score1: ");
+       int score1 = int.Parse(Console.ReadLine()); //score1 จากกรรมการคนแรก ผู้ชาย
+
+       Console.Write("Input score2: ");
+       int score2 = int.Parse(Console.ReadLine()); //score2 จากกรรมการคนแรก ผู้หญิง
+
+       if (N >= 1000)
+    }
+}
